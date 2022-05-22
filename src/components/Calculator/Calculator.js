85cbe82c485e8448026aa56efcfb10e0c5c7ff55
DavidLageiste <lageiste.david@gmail.com>
2022-05-22 19:44:26 +0100

    add calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -4,6 +4,10 @@ import { useRef, useState } from 'react';
 
 function Calculator({calculatorToggled, setCalculatorToggled, zIndexes, setZIndexes}){
     const calculatorRef = useRef(null);
+    const [lastResult, setLastResult] = useState(null);
+    const [leftOperator, setLeftOperator] = useState(null);
+    const [rightOperator, setRightOperator] = useState(null);
+    const [result, setResult] = useState(null);
 
     function handleClick(){
         setZIndexes({
@@ -18,6 +22,14 @@ function Calculator({calculatorToggled, setCalculatorToggled, zIndexes, setZInde
         setCalculatorToggled(false);
     }
 
+    function getResult(){
+        if(!rightOperator || (!leftOperator && "test" == 1)) {
+            return;
+        }
+        window.eval();
+    }
+
+
     return (
         <Draggable handle=".status-bar">
             <div style={{'zIndex': zIndexes['calculator']}} className={`calculator-window ${calculatorToggled ? 'visible' : ''}`} ref={calculatorRef} onClick={handleClick} >
@@ -27,6 +39,35 @@ function Calculator({calculatorToggled, setCalculatorToggled, zIndexes, setZInde
                     <div className="close-btn" onClick={closeCalculator} >X</div>
                 </div>
                 <div className="Calculator">
+                    <div className="result">{result}</div>
+                    <div className="keypad">
+                        <table>
+                            <tr>
+                                <td className="">/</td>
+                                <td className="">*</td>
+                                <td className="">+</td>
+                                <td className="">-</td>
+                            </tr>
+                            <tr>
+                                <td id="9">9</td>
+                                <td id="8">8</td>
+                                <td id="7">7</td>
+                                <td onClick={getResult}>=</td>
+                            </tr>
+                            <tr>
+                                <td id="6">6</td>
+                                <td id="5">5</td>
+                                <td id="4">4</td>
+                                <td id="CE">CE</td>
+                            </tr>
+                            <tr>
+                                <td id="3">3</td>
+                                <td id="2">2</td>
+                                <td id="1">1</td>
+                                <td id="C">C</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </Draggable>
--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -5,7 +5,7 @@ import Notepad from './Notepad/Notepad';
 import Calculator from './Calculator/Calculator';
 import FileExplorer from './FileExplorer/FileExplorer';
 import './Desktop.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Draggable from 'react-draggable';
 
 function Desktop({ isLoading }){
@@ -19,6 +19,7 @@ function Desktop({ isLoading }){
     const [notepadToggled, setNotepadToggled] = useState(false);
     const [calculatorToggled, setCalculatorToggled] = useState(false);
     const [fileExplorerToggled, setFileExplorerToggled] = useState(false);
+    const [selected, setSelected] = useState(null);
 
     function openNotepad(){
         setNotepadToggled(true);
@@ -35,25 +36,27 @@ function Desktop({ isLoading }){
         setZIndexes({...zIndexes, "calculator": 4})
     }
 
+    useEffect(() => {
+    }, []);
     return(
         <div className={`Desktop ${isLoading ? '' : 'visible'}`}>
             <TopBar />
             <SideBar itemsToggled={{consoleToggled, notepadToggled, calculatorToggled}} setConsoleToggled={setConsoleToggled} setNotepadToggled={setNotepadToggled} setCalculatorToggled={setCalculatorToggled}/>
             <Draggable>
-                <div className="desktop-icon notepad" onDoubleClick={openNotepad}>
-                    <img src="/notepad.png" />
+                <div className={`desktop-icon notepad${selected === "notepad" ? " selected" : ""}`} id="notepad" onClick={(e) => { setSelected(e.currentTarget.id); } } onDoubleClick={openNotepad}>
+                    <div className="icon"><img alt="icon-desktop" src="/notepad.png" /></div>
                     <div className="icon-name">About me</div>
                 </div>
             </Draggable>
             <Draggable>
-                <div className="desktop-icon calculator" onDoubleClick={openCalculator}>
-                    <img src="/calculator.png" />
+                <div className={`desktop-icon calculator${selected === "calculator" ? " selected" : ""}`} id="calculator" onClick={(e) => { setSelected(e.currentTarget.id); } } onDoubleClick={openCalculator}>
+                    <div className="icon"><img alt="icon-desktop" src="/calculator.png" /></div>
                     <div className="icon-name">Calculator</div>
                 </div>
             </Draggable>
             <Draggable>
-                <div className="desktop-icon file-explorer" onDoubleClick={openFileExplorer}>
-                    <img src="/file-explorer.png" />
+                <div className={`desktop-icon file-explorer${selected === "file-explorer" ? " selected" : ""}`} id="file-explorer" onClick={(e) => { setSelected(e.currentTarget.id); } } onDoubleClick={openFileExplorer}>
+                    <div className="icon"><img alt="icon-desktop" src="/file-explorer.png" /></div>
                     <div className="icon-name">File explorer</div>
                 </div>
             </Draggable>
